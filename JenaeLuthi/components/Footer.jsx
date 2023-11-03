da4f052a6346f1c64692cb9af69ba6b19b5e4b29
Jenae Luthi <jenaeluthi@gmail.com>
2023-11-03 14:48:18 -0600

    added a header and footer component

---
+++ JenaeLuthi/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Jenae Luthi</p>
+    </footer>
+  );
+}
+
+export default Footer;
