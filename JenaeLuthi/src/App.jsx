a902f764406c5136eda4b715371b4f38e2a9694d
Jenae Luthi <jenaeluthi@gmail.com>
2023-11-03 15:40:51 -0600

    moved the component folder to correct spot. Commented out the CSS to see what I am working with.

--- JenaeLuthi/src/App.jsx
+++ JenaeLuthi/src/App.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 import './App.css';
-import Header from '../components/Header'; // Import your Header component
-import Footer from '../components/Footer';
+import Header from './components/Header'; // Import your Header component
+import Footer from './components/Footer';
 function App() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="App">
         <Header /> 
         <div>
-          <a href="https://vitejs.dev" target="_blank">
+          {/* <a href="https://vitejs.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
           <a href="https://react.dev" target="_blank">
             <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+          </a> */}
         </div>
-        <h1>Vite + React</h1>
-        <div className="card">
+        <h1>About Me</h1>
+        {/* <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
           </button>
@@ -30,7 +30,7 @@ function App() {
         </div>
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
-        </p>
+        </p> */}
         <Footer />
       </div>
     </>
