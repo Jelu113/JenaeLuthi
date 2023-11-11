c64d984f40109cde2134e70d9390d3bf49d5fb56
Jenae Luthi <jenaeluthi@gmail.com>
2023-11-11 08:43:46 -0700

    added gradient color to the header and fixed the send button function on the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,97 +1,114 @@
-export default function Contact() {
-  return (
-   <>
-        <section className="mb-4 contact">
-        <h2>Contact me</h2>
+import React, { useState } from "react";
 
-          <p> 
+export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform additional actions here before or after submitting the form
+    // For example, you can send the form data to a server or perform client-side validation.
+    console.log("Form submitted:", formData);
+  };
+
+  return (
+    <>
+      <section className="mb-4 contact">
+        <div className="container">
+          <h2>Contact me</h2>
+
+          <p>
             Do you have any questions? Please do not hesitate to contact me
             directly.{" "}
           </p>
-          
-              <form
-                id="contact-form"
-                name="contact-form"
-                action="mail.php"
-                method="POST"
-              >
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="md-form mb-0">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                      ></input>
-                      <label htmlFor="name" className="">
-                        Your name
-                      </label>
-                    </div>
-                  </div>
 
-                  <div className="col-md-6">
-                    <div className="md-form mb-0">
-                      <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                      ></input>
-                      <label htmlFor="email" className="">
-                        Your email
-                      </label>
-                    </div>
-                  </div>
+          <form
+            id="contact-form"
+            name="contact-form"
+            onSubmit={handleSubmit}
+          >
+            <div className="row">
+              <div className="col-md-6">
+                <div className="md-form mb-0">
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="form-control"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="name" className="">
+                    Your name
+                  </label>
                 </div>
-
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="md-form mb-0">
-                      <input
-                        type="text"
-                        id="subject"
-                        name="subject"
-                        className="form-control"
-                      ></input>
-                      <label htmlFor="subject" className="">
-                        Subject
-                      </label>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="row">
-                   <div className="col-md-12"> 
-                    <div className="md-form">
-                      <textarea
-                        type="text"
-                        id="message"
-                        name="message"
-                        rows="2"
-                        className="form-control md-textarea"
-                      ></textarea>
-                      <label htmlFor="message">Your message</label>
-                    </div>
-                  </div>
-                </div>
-              </form>
-
-              <div className="text-center text-md-center">
-                <a
-                  className="btn btn-primary"
-                  onclick="document.getElementById('contact-form').submit();"
-                >
-                  Send
-                </a>
               </div>
-              <div className="status"></div>
-       
-        </section>
-        
-      
-  
+
+              <div className="col-md-6">
+                <div className="md-form mb-0">
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="email" className="">
+                    Your email
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-12">
+                <div className="md-form mb-0">
+                  <input
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    className="form-control"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="subject" className="">
+                    Subject
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-12">
+                <div className="md-form">
+                  <textarea
+                    type="text"
+                    id="message"
+                    name="message"
+                    rows="2"
+                    className="form-control md-textarea"
+                    onChange={handleChange}
+                  ></textarea>
+                  <label htmlFor="message">Your message</label>
+                </div>
+              </div>
+            </div>
+
+            <div className="text-center text-md-center">
+              <button type="submit" className="btn btn-primary">
+                Send
+              </button>
+            </div>
+            <div className="status"></div>
+          </form>
+        </div>
+      </section>
     </>
-    
   );
 }
